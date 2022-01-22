d7ce1ec3b13f7f66ff6aa24fa24795c2408c68f6
Nikolai von Krusenstiern <nikolai.krusenstiern@esa.int>
2022-01-22 01:19:13 +0100

    add test_make_shields.py

--- tests/__dev_tools/input_query/test_make_shields.py
+++ tests/__dev_tools/input_query/test_make_shields.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
+"""Test input parsing and eval functions with pytest."""
 
-...  # noqa: WPS428
+import pytest  # pylint: disable=E0401
+
+from dev_tools.meters.make_shields import Shield  # pylint: disable=E0401
+
+
+@pytest.mark.parametrize(  # noqa: WPS317
+    ("expected"),
+    [
+        ("/Users/nkrusens/esabox/Developer/python/cli-calc"),
+    ],
+)
+class TestMakeShields:  # pylint: disable=R0903
+    """Test make Shield."""
+
+    @staticmethod
+    def test_provide_work_dir(  # noqa: WPS602
+        expected: str,
+    ):
+        """A pwd test."""
+        assert Shield.provide_work_dir() == expected
