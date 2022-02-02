b93ddded3590c74c4a0fb1ddd4a32bed2014e92c
Nikolai von Krusenstiern <nikolai.krusenstiern@esa.int>
2022-02-02 02:25:13 +0100

    adds help line

--- dev_tools/sem_ver/get_ver_for_rule.py
+++ dev_tools/sem_ver/get_ver_for_rule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Bump Current version."""
+"""Print next Version for given Bump Rule."""
 
 import os
 import sys
@@ -10,7 +10,9 @@ import semver  # pylint disable=E0401
 
 def usage() -> None:
     """Usage."""
-    print('Usage: get_ver_for_rule.py for "patch"')  # noqa: WPS421
+    print('Prints next Version for given Bump Rule')  # noqa: WPS421
+    print('Usage: get_ver_for_rule.py: "defaults to patch"')  # noqa: WPS421
+    print('   Or: get_ver_for_rule.py patch')  # noqa: WPS421
     print("   Or: get_ver_for_rule.py minor")  # noqa: WPS421
     print("   Or: get_ver_for_rule.py major")  # noqa: WPS421
     sys.exit()
