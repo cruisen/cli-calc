f556d13c0919dd73b579d97d81313be773813c73
Nikolai von Krusenstiern <nikolai.krusenstiern@esa.int>
2022-02-03 01:18:57 +0100

    refactors all lines with flake8 infos

--- dev_tools/sem_ver/check_docs_requirements.py
+++ dev_tools/sem_ver/check_docs_requirements.py
@@ -5,8 +5,6 @@
 import os
 import sys
 
-import semver  # pylint disable=E0401
-
 
 def usage() -> None:
     """Usage."""
@@ -14,31 +12,30 @@ def usage() -> None:
     print("Usage: check_docs_requirements.py")  # noqa: WPS421
     sys.exit()
 
-print("checking: docs/requirements.txt")
 
-cmd_dev = os.popen(
-    "poetry export --without-hashes --dev --format requirements.txt | cut -d';' -f1"
+print("checking: docs/requirements.txt")  # noqa: WPS421
+
+cmd_dev = os.popen(  # noqa: S605,S607
+    "poetry export --without-hashes --dev --format requirements.txt | cut -d';' -f1",
 )  # noqa: S605, S607
 dev: str = cmd_dev.read().strip()
 
-cmd_doc = os.popen(
-    'cd $(git rev-parse --show-toplevel) ; cat docs/requirements.txt | grep -v "^$" | grep -v "^#" | sort'
-)  # noqa: S605, S607
-doc: str = cmd_doc.readlines()
+cmd_doc = os.popen(  # noqa: S605,S607
+    'cd $(git rev-parse --show-toplevel) ; cat docs/requirements.txt | grep -v "^$" | grep -v "^#" | sort',  # noqa: E501
+)
+doc = cmd_doc.readlines()
 
 
 for line_nl in doc:
-    line=line_nl.strip()
-    package=line.split("=")[0]
+    line = line_nl.strip()
+    package = line.split("=")[0]
 
-    for item in dev.split("\n"):
-        if package in item:
-            match_line=item.strip()
-            match=match_line.split("=")[0]
+    for dev_line in dev.split("\n"):
+        if package in dev_line:
+            match_line = dev_line.strip()
+            match = match_line.split("=")[0]
 
             if match == package:
                 if match_line != line:
-                    print(match_line)
-                    print(line)
-
-
+                    print(match_line)  # noqa: WPS421
+                    print(line)  # noqa: WPS421
