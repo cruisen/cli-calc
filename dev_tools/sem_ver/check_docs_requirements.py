cf8d2b941ae2cabd76d1bff1fdb958d92afb8f2d
Nikolai von Krusenstiern <nikolai.krusenstiern@esa.int>
2022-02-16 12:46:09 +0100

    updates mistune (0.8.4 -> 2.0.2) and m2r2 (0.3.2 -> 0.3.1). fixes check_docs_requirements.py to handle comments and greater than as well as find missing entries.

--- dev_tools/sem_ver/check_docs_requirements.py
+++ dev_tools/sem_ver/check_docs_requirements.py
@@ -3,6 +3,7 @@
 """Check docs/requirement.txt."""
 
 import os
+import re
 import sys
 
 
@@ -21,21 +22,30 @@ cmd_dev = os.popen(  # noqa: S605,S607
 dev: str = cmd_dev.read().strip()
 
 cmd_doc = os.popen(  # noqa: S605,S607
-    'cd $(git rev-parse --show-toplevel) ; cat docs/requirements.txt | grep -v "^$" | grep -v "^#" | sort',  # noqa: E501
+    'cd $(git rev-parse --show-toplevel) ; cat docs/requirements.txt | grep -v "^$" | grep -v "^#" | sed "s/#.*//" | sort',  # noqa: E501
 )
 doc = cmd_doc.readlines()
 
 
 for line_nl in doc:
     line = line_nl.strip()
-    package = line.split("=")[0]
+    package = re.split("=|<|>", line)[0]
 
+    found = 0
     for dev_line in dev.split("\n"):
-        if package in dev_line:
-            match_line = dev_line.strip()
-            match = match_line.split("=")[0]
+        if package not in dev_line:
+            continue
 
-            if match == package:
-                if match_line != line:
-                    print(match_line)  # noqa: WPS421
-                    print(line)  # noqa: WPS421
+        found += 1
+        match_line = dev_line.strip()
+        match = re.split("=|<|>", match_line)[0]
+
+        if match != package:
+            continue
+
+        if match_line != line:
+            print(match_line)  # noqa: WPS421
+            print(line)  # noqa: WPS421
+
+    if not found:
+        print(f"In doc only: {line}")
