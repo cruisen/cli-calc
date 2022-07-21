213ea8b7d96f8a3779822fa60a007cea131aa773
nkrusens <nikolai.krusenstiern@esa.int>
2022-07-21 15:56:14 +0200

    Add dev_tools/print_help/prompt.txt.

--- dev_tools/print_help/print_help.py
+++ dev_tools/print_help/print_help.py
@@ -17,13 +17,22 @@ from cli_calc.cli_output import Output  # pylint: disable=C0413  # noqa: E402
 from cli_calc.config import Config  # pylint: disable=C0413  # noqa: E402
 from cli_calc.memory import Memory  # pylint: disable=C0413  # noqa: E402
 
+help_file_name: str = "help.txt"
+prompt_file_name: str = "prompt.txt"
+
 
 def main() -> None:  # pragma: no cover
     """Main."""
     Config.init(Memory.value_dict)
-    with open("help.txt", "w") as file_handle:
+    with open(help_file_name, "w") as file_handle:
         with redirect_stdout(file_handle):
             Output.print_help()
+
+    with open(help_file_name, "r") as file_handle:  # noqa: WPS440
+        last_line: str = file_handle.readlines()[-1]
+
+    with open(prompt_file_name, "w") as file_handle:  # noqa: WPS440
+        file_handle.writelines(last_line)
 
 
 if __name__ == "__main__":  # pragma: no cover
